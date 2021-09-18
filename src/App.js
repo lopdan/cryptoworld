971392409ff7c00a4e29701c76c73fdc358778e3
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-09-19 00:12:45 +0200

    NavigatonBar file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Switch, Route, Link} from 'react-router-dom';
+import { Layout, Typography, Space} from 'antd';
 
 const App = () => {
 	return (
-		<div>
-
+		<div>	
 		</div>
 	)
 }
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar} from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@antd-design/icons';
+
+const NavigationBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavigationBar
