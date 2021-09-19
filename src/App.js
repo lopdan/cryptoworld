fadacce472cb69e86aec530580b2b5db7c50079d
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-09-19 19:31:46 +0200

    Starting layout items

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import React from 'react'
 import { Switch, Route, Link} from 'react-router-dom';
 import { Layout, Typography, Space} from 'antd';
 
+import { NavigationBar } from './components';
+import './App.css';
+import 'antd/dist/antd.css';
+
+
+//9300C5
 const App = () => {
 	return (
-		<div>	
-		</div>
+		<div className="app">
+			<div className="navbar">
+				<NavigationBar />
+			</div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						<Switch>
+							<Route exact path="/">
+
+							</Route>
+						</Switch>
+					</div>
+				</Layout>
+			</div>
+			<div className="footer">
+					<Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021 
+						<Link to="/">
+							 CryptoWorld Inc.
+						</Link> <br />
+						All Rights Reserved.
+					</Typography.Title>
+					<Space>
+						<Link to="/">Home</Link>
+						<Link to="/exchanges">Exchanges</Link>
+						<Link to="/news">News</Link>
+					</Space>
+				</div>
+  	</div>
 	)
 }
 
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Button, Menu, Typography, Avatar} from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@antd-design/icons';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
 
 const NavigationBar = () => {
     return (
-        <div>
-            
-        </div>
+		<div className="nav-container">
+			<div className="logo-container">
+				<Avatar src="./bitcoin-mind.png" size="large"/>
+				<Typography.Title level={2} className="logo">
+					<Link to="/">CryptoWorld</Link>
+				</Typography.Title>
+			</div>
+		</div>
     )
 }
 
-export default NavigationBar
+export default NavigationBar;
