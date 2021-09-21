2cc74770d80cc43fa8e447d492ffa955696a6a6f
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-09-21 22:55:47 +0200

    Homepage general data from API

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,17 +11,20 @@ const { Title } = Typography;
 
 const HomePage = () => {
 	const { data, isFetching } = useGetCryptosQuery();
+	const globalStats = data?.data.stats;
+
 	console.log(data);
+	if(isFetching) return <Loader/>;
 
 	return(
 		<>
 			<Title level={2} className="heading">Global Crypto Stats</Title>
 			<Row>
-				<Col span={12}><Statistic title="Total Cryptocurrencias" value="5"/></Col>
-				<Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-				<Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
-				<Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
-				<Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+				<Col span={12}><Statistic title="Total Cryptocurrencias" value={globalStats.total}/></Col>
+				<Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+				<Col span={12}><Statistic title="Total Market Cap ($)" value={millify(globalStats.totalMarketCap)}/></Col>
+				<Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+				<Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
 			</Row>
 			<Cryptocurrencies simplified />
 		</>
