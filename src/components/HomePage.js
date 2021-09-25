45bfc2b02946e551b7030e850074eebf48ee90d0
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-09-25 16:47:48 +0200

    Home container style fix

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -17,14 +17,16 @@ const HomePage = () => {
 
 	return(
 		<>
-			<Title level={2} className="heading">Global Crypto Stats</Title>
-			<Row>
-				<Col span={12}><Statistic title="Total Cryptocurrencias" value={globalStats.total}/></Col>
-				<Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
-				<Col span={12}><Statistic title="Total Market Cap ($)" value={millify(globalStats.totalMarketCap)}/></Col>
-				<Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
-				<Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
-			</Row>
+			<div className="home-heading-container">
+				<Title level={2} className="heading">Global Crypto Stats</Title>
+				<Row>
+					<Col span={12}><Statistic title="Total Cryptocurrencias" value={globalStats.total}/></Col>
+					<Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+					<Col span={12}><Statistic title="Total Market Cap ($)" value={millify(globalStats.totalMarketCap)}/></Col>
+					<Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+					<Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+				</Row>
+			</div>
 			<div className="home-heading-container">
 				<Title level={2} className="home-title">Top 10 Cryptocurrencies</Title>
 				<Cryptocurrencies simplified />
