618512e43e54a6d6adb98233a8ed881109b30b4b
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-10-02 23:51:36 +0200

    Initial design coin data

--- src/components/CoinData/CoinData.jsx
+++ src/components/CoinData/CoinData.jsx
@@ -2,25 +2,53 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
+import './CoinData.scss';
 
 import { useGetCoinDataQuery } from '../../api/cryptoApi';
 import Loader from './../Loader';
 
 
+const { Title } = Typography;
+
 const CoinData = () => {
   const { coinId } = useParams();
   const [ timeperiod, setTimeperiod ] = useState('7d');
   const { data, isFetching } = useGetCoinDataQuery(coinId);
+  const cryptoDetails = data?.data?.coin;
 
 	console.log(data)
 
   if (isFetching) return <Loader />;
 
-  return (
-    <div>
+  const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
-    </div>
+  const stats = [
+    { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+    { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+    { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+    { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+    { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+  ];
+
+  const genericStats = [
+    { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+    { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+    { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+    { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+    { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+  ];
+
+  return (
+    <Col>
+      <Col className="coin-heading-container">
+        <Title level={2} className="coin-name">
+          {data?.data?.coin.name} ({data?.data?.coin.slug}) Price
+        </Title>
+        <p>{cryptoDetails.name} live price in US Dollar (USD). View value statistics, market cap and supply.</p>
+      </Col>
+    </Col>
   );
 };
 
