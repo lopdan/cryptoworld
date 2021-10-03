59a153f2093f06a33541d5d99964486e6930150c
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-10-03 16:57:19 +0200

    Chart file

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import './Chart.scss';
+
+
+const Chart = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Chart;
