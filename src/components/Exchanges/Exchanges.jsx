6fa3e721c9780d66a8edfc6e829c806de88bed25
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-10-06 23:10:50 +0200

    Exchange table width reduced by 10%

--- src/components/Exchanges/Exchanges.jsx
+++ src/components/Exchanges/Exchanges.jsx
@@ -31,10 +31,10 @@ const Exchanges = () => {
     <div className="exchange-table-container">
       <Row>
         <Row style={{ width: '100%'}}>
-          <Col span={5} style={{fontWeight: 700, fontSize: '24px', marginLeft: '2%'}}>Exchanges</Col>
-          <Col span={5} style={{fontWeight: 700, fontSize: '24px'}}>24h Trade Volume</Col>
-          <Col span={5} style={{fontWeight: 700, fontSize: '24px', marginLeft: '4%'}}>Markets</Col>
-          <Col span={5} style={{fontWeight: 700, fontSize: '24px', marginLeft: '4%'}}>Change</Col>
+          <Col span={9} style={{fontWeight: 500, fontSize: '24px', marginLeft: '0%'}}>Exchanges</Col>
+          <Col span={5} style={{fontWeight: 500, fontSize: '24px', marginLeft: '-2%'}}>24h Trade Volume</Col>
+          <Col span={5} style={{fontWeight: 500, fontSize: '24px', marginLeft: '0%'}}>Markets</Col>
+          <Col span={5} style={{fontWeight: 500, fontSize: '24px', marginLeft: '0%'}}>Change</Col>
         </Row>
         {exchangesList.map((exchange) => (
           <Col span={24}>
@@ -43,15 +43,15 @@ const Exchanges = () => {
                 key={exchange.id}
                 showArrow={false}
                 header={(
-                  <Row key={exchange.id}>
-                    <Col span={6} style={{fontWeight: 700}}>
+                  <Row key={exchange.id} style={{display: 'flex', justifyContent: 'space-between'}}> 
+                    <Col span={9} style={{fontWeight: 700}}>
                       <Text><strong>{exchange.rank}.</strong>       </Text>
                       <Avatar className="exchange-image" src={exchange.iconUrl} />
                       <Text ><strong>   {exchange.name}</strong></Text>
                     </Col>
-                    <Col span={6} style={{fontWeight: 600}}>${millify(exchange.volume)}</Col>
-                    <Col span={6} style={{fontWeight: 600}}>{millify(exchange.numberOfMarkets)}</Col>
-                    <Col span={6} style={{fontWeight: 600}}>{millify(exchange.marketShare)}%</Col>
+                    <Col span={5} style={{fontWeight: 600}}>${millify(exchange.volume)}</Col>
+                    <Col span={5} style={{fontWeight: 600}}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={5} style={{fontWeight: 600}}>{millify(exchange.marketShare)}%</Col>
                   </Row>
                   )}
               >
